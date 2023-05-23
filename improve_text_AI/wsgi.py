d076bfc3b6375178b36d5391deb4512acb47d812
Illia Khorunzhyi <bkmzilia@gmail.com>
2023-05-23 12:27:23 +0300

    init

---
+++ improve_text_AI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for improve_text_AI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'improve_text_AI.settings')
+
+application = get_wsgi_application()
